445d38605311310e293696cde2825694e5513445
Luay Matar <luaymatar2001@gmail.com>
2026-09-22 20:31:13 +0300

    search input fix3

--- app/Livewire/HeadHouseholdsTable.php
+++ app/Livewire/HeadHouseholdsTable.php
@@ -253,6 +253,8 @@ final class HeadHouseholdsTable extends PowerGridComponent
             Filter::inputText('num_Family_Members'),
             Filter::inputText('legal_confirmation'),
             Filter::inputText('Date_partner_martyrdom'),
+            //Filter Date of birth
+            Filter::inputText('Date_birth')->dateTimeFormat('Y-m-d'),
             Filter::inputText('status_document'),
             Filter::inputText('widow_identity'),
             Filter::inputText('updated_at'),
